213eb065c9656b7660c949a990c2139168e354d5
Chris Allmark <chris.allmark@gmail.com>
2022-08-17 12:21:38 +0100

    Update scripts

--- configure.sh
+++ configure.sh
@@ -4,4 +4,4 @@ if [[ $# -ne 1 ]]; then
   echo "Usage: configure <docker hub username>"
   exit 1
 fi
-sed -i '' "s/DOCKER_HUB_USERNAME/${DOCKER_HUB_USERNAME}/g" infra/*.yaml
+find ./ -name '*.yaml' -exec sed -i '' "s/DOCKER_HUB_USERNAME/${DOCKER_HUB_USERNAME}/g" {} \;
